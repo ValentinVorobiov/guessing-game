c162ed9dc3c517e9efb47ee8238289827147c89c
Varabiou Valiantsin <vxdator@gmail.com>
2019-10-03 10:17:40 +0300

    feat: done Guessing Game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,37 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+
+        this.minValue = 0;
+        this.maxValue = 0;
+        this.currentGuess = null;
+
+    }
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
 
+        this.currentGuess = Math.floor( 
+            Math.round( 
+                0.5*( this.minValue + this.maxValue ) 
+            ) 
+        );
+        return this.currentGuess;
+
     }
 
     lower() {
-
+        this.maxValue = this.currentGuess;
+        this.currentGuess = null;
     }
 
     greater() {
-
+        this.minValue = this.currentGuess;
+        this.currentGuess = null;
     }
 }
 
